2f49dfcb765cc4f54df574bee8e99bd5647b3678
Rakesh <dr8452799@gmail.com>
2025-07-03 11:15:29 +0530

    Create Abstraction.java

---
+++ OOPS/Abstraction.java
@@ -0,0 +1,40 @@
+package oops;
+/*
+ * Abstraction is the Process of Hiding the Implementation & Showing the Necessarry Information to the User.
+ * Abstraction Method ---> It consists only Method Signature no body.
+ * Abstract Class ---> It is Restricted We can't create Objects to that class.
+ * -->Real Time Example--> Phone Pay Transaction Between User & Recevier
+ * ------->But we Don't Know How the Internal Process how it  Will Work
+ * --------> The Internal Process Will Hide & showing Necessary Information to the user.
+ * 
+ *         <---Below are Rules of Abstraction---> 
+ * ---> If in a class has one Abstract Method We Must Decalre it has as a Abstract.
+ * ---> Abstraction have both Abstract Methods nd Regular Methods.
+ * ---> Abstract nd Final Keywords are'nt Use Together.
+ * ---> Abstract class Can Inherit from Another Abstract Class
+
+ * Difference between Abstraction and Interfaces
+ *  Abstraction can have constructors and interface dont have constructors
+ *  Abstraction have both abstract methods and concerte methods, will Interfaces have only Pure Abstract methods
+ * 
+ */
+abstract class animal{
+	abstract void makesound(); // Abstract method Signature
+}
+class Dog extends animal
+{
+	 void makesound() // Must implement the inherited abstract method
+	{
+		System.out.println("woof!");
+	}
+}
+
+public class Abstraction {
+
+	public static void main(String[] args) {
+		Dog D = new Dog();
+		D.makesound();
+
+	}
+
+}
